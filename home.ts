38feab24c45a419b24c79bb9d6f2bd51f5d84f2b
Kachman21 <Kachman21onyedikachiokafor21@gmail.com>
2022-08-10 11:02:28 +0100

    Create indx.js and home.ts, added some variables

---
+++ home.ts
@@ -0,0 +1,7 @@
+const firstName: string = "john";
+let age: number = 30;
+let lastname: string = "Hunter" ;
+console.warn("lastname variable  is in camel case", lastname);
+if (typeof (age) !== "number") {
+    console.error("Age is not a number");
+}
